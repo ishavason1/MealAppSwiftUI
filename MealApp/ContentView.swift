92ccd6a0f0787def8625699e31148af558f87984
Isha Vason <isha.vason@tothenew.com>
2020-09-06 12:06:44 +0530

    Initial Commit

---
+++ MealApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealApp
+//
+//  Created by Isha on 06/09/20.
+//  Copyright © 2020 Isha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
