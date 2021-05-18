96de89e8fe481ce5345626f4339f9f0dd0e815b2
Isha Vason <isha.vason@tothenew.com>
2021-05-18 18:56:15 +0530

    Binding Animation to list on home page and binding data to the views

--- MealApp/View/ContentView.swift
+++ MealApp/View/ContentView.swift
@@ -14,27 +14,80 @@ struct ContentView: View {
     @ObservedObject var viewModel: CategoryListViewModel
     @ObservedObject var viewFoodModel: CategoryFoodListViewModel
     @State private var showingDetail = false
+   
     var body: some View {
         NavigationView {
             ZStack {
                 Color.appBackgroundColor.edgesIgnoringSafeArea(.all)
-               
-                ASCollectionView(data: viewModel.categoriesData, dataID: \.self) { item, _ in
-                    
-                    NavigationLink(destination: CategoriesFoodList(categorytitle: item.strCategory, viewModel: self.viewFoodModel)) {
-                         CollectionView(dataItems: item, imageLoader: ImageLoader(urlString: item.strCategoryThumb)).padding(.top)
-                    }.buttonStyle(PlainButtonStyle())
-                   
-                }
-                .layout {
-                    .grid(layoutMode: .adaptive(withMinItemSize: 160),
-                          itemSpacing: 20,
-                          lineSpacing: 20,
-                          itemSize: .absolute(160))
-                    }
+                BreakFastMenu()
+//                ASCollectionView(data: viewModel.categoriesData, dataID: \.self) { item, _ in
+//
+//                    NavigationLink(destination: CategoriesFoodList(categorytitle: item.strCategory, viewModel: self.viewFoodModel)) {
+//                         CollectionView(dataItems: item, imageLoader: ImageLoader(urlString: item.strCategoryThumb)).padding(.top)
+//                    }.buttonStyle(PlainButtonStyle())
+//
+//                }
+//                .layout {
+//                    .grid(layoutMode: .adaptive(withMinItemSize: 160),
+//                          itemSpacing: 20,
+//                          lineSpacing: 20,
+//                          itemSize: .absolute(160))
+//                    }
                 
-            }.navigationBarTitle("Categories", displayMode: .large)
-                .onAppear(perform: viewModel.loadData)
+            }
+            .navigationBarTitle("Menu", displayMode: .large)
+           
+                //.onAppear(perform: viewModel.loadData)
+            
+        }
+    }
+    
+}
+
+struct BreakFastMenu: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    private func getScale(proxy: GeometryProxy) -> CGFloat {
+        var scale: CGFloat = 1
+        let x = proxy.frame(in: .global).minX
+        let diff = abs(x)
+        if diff < 100 {
+            scale = 1 + (100 - diff) / 500
+        }
+        return scale
+    }
+    var body: some View {
+        ScrollView {
+            
+           
+            VStack(alignment: .leading) {
+                            Text("Breakfast")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                            
+                        
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack (spacing: 30){
+                        ForEach(menu[0].items) { num in
+                            GeometryReader { proxy in
+                                let scale = getScale(proxy: proxy)
+                                Image(num.mainImage)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 200)
+                                    .clipped()
+                                    .cornerRadius(5)
+                                    .shadow(radius: 5 )
+                                  .scaleEffect(CGSize(width: scale, height: scale))
+                            }
+                                
+                            .frame(width: 200, height: 200)
+                               
+                        }
+                    }.padding()
+                        
+                }
+            }.padding()
+            
             
         }
     }
